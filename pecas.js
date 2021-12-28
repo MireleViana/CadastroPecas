bd59f5c5d00d43732b04b4b9a2b130029589e263
Mirele Viana <mirele21@gmail.com>
2021-12-27 21:31:40 -0300

    Adicionando código de cadastro de peças - Aula Remota 1 de Lógica

---
+++ pecas.js
@@ -0,0 +1,20 @@
+let pesoPeca = 110
+if (pesoPeca > 100) {
+    console.log("Peso maior que 100g, assim, podemos caastrar a peça!")
+} else {
+    console.log("Peso insuficiente, não é possível cadastrar a peça!")
+}
+let numeroPecas = 11
+
+if (numeroPecas <10) {
+    console.log("Ainda há espaço na caixa, podemos cadastrar a peça!")           
+} else {
+    console.log("Não há espaço disponível na caixa, não podemos cadastrar a peça!")
+}
+let nomePeca = 'Disco de freio'
+console.log("O comprimento da peça é: ", nomePeca.length)
+if (nomePeca.length < 3) {
+    console.log("Nome de peça muito curto, não é possível cadastrar!")
+} else {
+    console.log("Nome de peça adequado, podemos cadastrar!")
+}
